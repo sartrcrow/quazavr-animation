83b0f0a6e48c53f5d46887289eeaeef33510e4d0
Julia Mnizhek <j.mnizhek@semrush.com>
2020-04-02 18:38:40 +0300

    Add spikes draft animation

--- js/quazavr-animation.js
+++ js/quazavr-animation.js
@@ -4,10 +4,17 @@ function animation() {
             rightFlipper = "#rightFlipper",
             rightWhisker = "#rightWhisker",
             leftWhisker = "#leftWhisker",
-            spaceGuy = "#spaceGuy",
-            quazavr = "#quazavr"
+            quazavr = "#quazavr",
+            spike1 = "#spike1",
+            spike2 = "#spike2",
+            spike3 = "#spike3",
+            spike4 = "#spike4",
+            spike5 = "#spike5",
+            spike6 = "#spike6"
 
     const S = .17
+
+    // Flippers animation
 
     const leftFlipperTL = gsap
           .timeline({
@@ -31,6 +38,8 @@ function animation() {
          })
           .yoyo(true)
 
+          // SpaceGuy whiskers animation
+
           const rightWhiskerTl = gsap
           .timeline({
               repeat: -1,
@@ -53,6 +62,8 @@ function animation() {
          })
           .yoyo(true)
 
+         // Quazavr movement up & down animation
+
           const quazavrTL = gsap
           .timeline({ 
               repeat: -1, 
@@ -62,5 +73,73 @@ function animation() {
                 y: 2
             })
         .yoyo(true)
+
+        //Spikes animation
+
+        const spike1TL = gsap
+          .timeline({
+              repeat: -1,
+              defaults: { transformOrigin: "0% 100%", ease: "none", duration: S * 2 }
+          })
+          .to(spike1, { 
+              rotate: 3,
+              duration: 0.9,
+         })
+          .yoyo(true)
+
+          const spike2TL = gsap
+          .timeline({
+              repeat: -1,
+              defaults: { transformOrigin: "0% 100%", ease: "none", duration: S * 2 }
+          })
+          .to(spike2, { 
+              rotate: 2,
+              duration: 0.9,
+         })
+          .yoyo(true)
+
+          const spike3TL = gsap
+          .timeline({
+              repeat: -1,
+              defaults: { transformOrigin: "0% 100%", ease: "none", duration: S * 2 }
+          })
+          .to(spike3, { 
+              rotate: 2,
+              duration: 0.9,
+         })
+          .yoyo(true)
+
+          const spike4TL = gsap
+          .timeline({
+              repeat: -1,
+              defaults: { transformOrigin: "0% 100%", ease: "none", duration: S * 2 }
+          })
+          .to(spike4, { 
+              rotate: 2,
+              duration: 0.9,
+         })
+          .yoyo(true)
+
+          const spike5TL = gsap
+          .timeline({
+              repeat: -1,
+              defaults: { transformOrigin: "0% 100%", ease: "none", duration: S * 2 }
+          })
+          .to(spike5, { 
+              rotate: 2,
+              duration: 0.9,
+         })
+          .yoyo(true)
+
+          const spike6TL = gsap
+          .timeline({
+              repeat: -1,
+              defaults: { transformOrigin: "0% 100%", ease: "none", duration: S * 2 }
+          })
+          .to(spike6, { 
+              rotate: 2,
+              duration: 0.9,
+         })
+          .yoyo(true)
 
 }
